6c61af44fb634b93350f905ef2ff477f7c4fa5f9
GTPSHAX <rafiehasannudin@gmail.com>
2024-09-30 21:08:08 +0700

    Add captcha

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const web = express();
 const cnf = require("./config.json");
 const path = require("path");
 const bodyParser = require('body-parser');
+const key = "6LexlVMqAAAAAMRSfyJizwDWSJGRtX0WOOuf3VOj";
 
 web.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -25,6 +26,24 @@ web.post('/player/login/dashboard', (req, res) => {
 });
 web.post('/player/growid/login/validate', (req, res) => {
     try {
+        const tkn = req.body['g-recaptcha-response'];
+        if (!tkn) {
+            return res.status(403).send('');
+        }
+        
+        const verifyURL = `https://www.google.com/recaptcha/api/siteverify?secret=${key}&response=${tkn}`;
+
+        axios.post(verifyURL)
+            .then(response => {
+                if (response.data.success) {
+                    res.send('CAPTCHA verified successfully!');
+                                                                                    } else {
+                                                                                                    res.status(400).send('CAPTCHA verification failed.');
+                                                                                                                }
+                                                                                                                        })
+                                                                                                                                .catch(error => {
+                                                                                                                                            res.status(500).send('Error verifying CAPTCHA');
+                                                                                                                                                    });
         const token = Buffer.from(
             `_token=${req.body._token}&growId=GROWPLUS&password=GROWPLUS`,
         ).toString('base64');
